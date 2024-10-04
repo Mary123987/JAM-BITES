4c738b8e250dcac8aa8a0182676fef71b5ea2367
Andrea <camposhuaman1276@gmail.com>
2024-10-04 03:45:18 +0000

    Catalogo modificacion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Catalogo()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
